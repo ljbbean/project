0f19bee2e96f4de23e837868390e113256bbf24d
ljb <ljbbean@163.com>
2018-05-21 20:09:55 +0800

    python做客户端自动刷新

---
+++ Fetch/autoFreshWeb.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from time import sleep
+
+browser = webdriver.Chrome("C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe")
+
+try:
+    while 1:
+        try:
+            button = browser.find_element_by_class_name("button-mod__primary___1tmFA")
+            button.click()
+            sleep(10)
+        except:
+            browser.get("https://trade.taobao.com/trade/itemlist/list_sold_items.htm")
+            sleep(10)
+            continue
+        pass
+finally:
+    browser.close() 
+
